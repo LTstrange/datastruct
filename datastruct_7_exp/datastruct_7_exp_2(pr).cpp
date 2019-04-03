8b1f6952dfad938a185b0a7e85b63882a259a43d
LTstrange <33590343+LTstrange@users.noreply.github.com>
2019-04-03 17:53:14 +0800

    增加了部分注释，未完成

--- datastruct_7_exp/datastruct_7_exp_2(pr).cpp
+++ datastruct_7_exp/datastruct_7_exp_2(pr).cpp
@@ -1,6 +1,26 @@
 #include<iostream>
 #include<string.h>
 using namespace std;
+
+/*
+ * 算法解释：
+ * 我们可以知道，在我们比对字符串的时候，如果前一个是不同的，那么后面的也就不用比对了。
+ *
+ * 这样来看的话，当我们组建一个以第一个字符串长度为行数，第二个字符串为列数的二维数组，
+ * 用来记录每个位置前面的匹配长度。例如：
+ *   a b c d e f
+ * b 0 1 0 0 0 0
+ * c 0 0 2 0 0 0
+ * d 0 0 0 3 0 0
+ * e 0 0 0 0 4 0
+ * c 0 0 1 0 0 0
+ * d 0 0 0 2 0 0
+ *
+ * 所以，当前位置的数值，取决于当前和斜上角。可用这个公式表达：
+ * label[i][j]
+ */
+
+
 int main()
 {
 	string a,b;
@@ -29,4 +49,10 @@ int main()
 	else
 	for(int p=max;p>0;p--)
 	cout<<a[h-p];
+	for (int i=0; i<=a.size(); i++){
+	    for (int j =0; j<=b.size(); j++){
+	        cout<<c[i][j]<<' ';
+	    }
+	    cout<<endl;
+	}
 	return 0;}
