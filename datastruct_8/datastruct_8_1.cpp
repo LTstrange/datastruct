ec790524a459b2ee93ab021b2f250bd5275d5bfa
LTstrange <835422774@qq.com>
2019-04-14 21:21:58 +0800

    finish!

--- datastruct_8/datastruct_8_1.cpp
+++ datastruct_8/datastruct_8_1.cpp
@@ -16,27 +16,49 @@ typedef struct {
 int main(){
     long r, c;
     int data, count=0;
-    TSMatrix matrix;
+    TSMatrix matrixA, matrixB;
     cin>>r>>c;
-    matrix.r = r;
-    matrix.c = c;
+    matrixA.r = r; matrixA.c = c;
     for (int i=0; i<r; i++){
         for (int j=0; j<c; j++){
             cin>>data;
             if (data){
-                matrix.data[count].data = data;
-                matrix.data[count].row = i;
-                matrix.data[count].col = j;
+                matrixA.data[count].data = data;
+                matrixA.data[count].row = i;
+                matrixA.data[count].col = j;
                 count++;
             }
         }
     }
-    matrix.len = count;
+    matrixA.len = count;
+    matrixB.len = matrixA.len;
+    matrixB.r = matrixA.c; matrixB.c = matrixA.r;
+    int num[c], position[c];
+    int col, t, p, q;
+    if (matrixB.len){
+        for (int col = 0; col < matrixA.c; ++col) {
+            num[col]=0;
+        }
+        for (t = 0; t < matrixA.len; ++t) {
+            num[matrixA.data[t].col]++;
+        }
+        position[0]=1;
+        for (col = 1; col < matrixA.c; ++col) {
+            position[col] = position[col-1]+num[col-1];
+        }
+        for (p = 0; p < matrixA.len; ++p) {
+            col = matrixA.data[p].col; q = position[col];
+            matrixB.data[q].row = matrixA.data[p].col;
+            matrixB.data[q].col = matrixA.data[p].row;
+            matrixB.data[q].data = matrixA.data[p].data;
+            position[col]++;
+        }
+    }
     count = 0;
-    for (int i = 0; i < r; ++i) {
-        for (int j = 0; j < c; ++j) {
-            if (matrix.data[count].row == i && matrix.data[count].col == j){
-                cout<<matrix.data[count].data<<' ';
+    for (int i = 0; i < matrixB.r; ++i) {
+        for (int j = 0; j < matrixB.c; ++j) {
+            if (matrixB.data[count].row == i && matrixB.data[count].col == j){
+                cout<<matrixB.data[count].data<<' ';
                 count++;
             } else {
                 cout<<"0 ";
