0da1e11d98b972724e88c45e7fb9c18a7a847a44
LTstrange <835422774@qq.com>
2019-04-14 20:00:02 +0800

    modify data_7 and data_8

--- datastruct_8/datastruct_8_1.cpp
+++ datastruct_8/datastruct_8_1.cpp
@@ -1,4 +1,22 @@
-//
-// Created by ltstrange on 19-4-14.
-//
+#include <iostream>
+using namespace std;
 
+#define MAXSIZE 1000
+
+typedef struct {
+    int row, col;
+    int data=0;
+} Triple;
+
+typedef struct {
+    Triple data[MAXSIZE+1];
+    int m, n, len;
+}TSMatrix;
+
+int main(){
+    int r, c;
+    TSMatrix matrix;
+
+    cin>>r>>c;
+    return 0;
+}
