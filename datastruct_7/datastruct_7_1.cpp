146dc89e8d7d8835c24705920d5572c0c9a9a9a7
LTstrange <835422774@qq.com>
2019-04-15 22:26:14 +0800

    finish a part of data_7_1.cpp

--- datastruct_7/datastruct_7_1.cpp
+++ datastruct_7/datastruct_7_1.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
+#include <string>
+#include <algorithm>
 using namespace std;
 
 int main(){
+    string a,b;
+    cin>>a;
+    cin>>b;
+    int m=a.size();
+    int n= b.size();
+    string c(m+n,'0');
+    int sum=0;
+    int p,q;
+    for (int i = 0; i < m; ++i) {
+        for (int j = 0; j < n; ++j) {
+            p = m-i + n-j;
+            sum = 
+        }
 
+    }
     return 0;
 }
