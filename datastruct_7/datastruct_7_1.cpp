882e30da7c95f9144bd3e5dcca94b3d8ec9153d7
LTstrange <835422774@qq.com>
2019-04-16 21:39:26 +0800

    finish data_7_1.cpp

--- datastruct_7/datastruct_7_1.cpp
+++ datastruct_7/datastruct_7_1.cpp
@@ -15,10 +15,16 @@ int main(){
     for (int i = 0; i < m; ++i) {
         for (int j = 0; j < n; ++j) {
             p = m-i + n-j;
-            sum = (a[m-i-1] - '0')*(b[n-j-1] - '0');
-            
+            sum += (a[m-i-1] - '0')*(b[n-j-1] - '0');
+            sum += c[p-1]-'0';
+            c[p-1] = sum%10 + '0';
+            sum = sum / 10;
         }
 
     }
+    while (*(c.begin()) == '0'){
+        c.erase(c.begin());
+    }
+    cout<<c<<endl;
     return 0;
 }
