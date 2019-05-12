700855abc9b17079d3fc62ac94715aed9963baef
LTstrange <835422774@qq.com>
2019-05-12 21:49:33 +0800

    finish the part of kruskal

--- datasturct_11-13/datastruct_11-13_2.cpp
+++ datasturct_11-13/datastruct_11-13_2.cpp
@@ -5,13 +5,19 @@ typedef struct {
     int from, to, dis;
 } Item;
 
-void Input(int vertexes, int edges, Item matrix[], int parent[])
+void Input(int edges, Item matrix[])
 {
     for (int i = 0; i < edges; ++i) {
         cin>>matrix[i].from>>matrix[i].to>>matrix[i].dis;
     }
+
+}
+
+void Init_tree(int vertexes, int parent[], int rank[])
+{
     for (int j = 0; j < vertexes; ++j) {
         parent[j] = -1;
+        rank[j] = 0;
     }
 }
 
@@ -29,13 +35,52 @@ void sort(int edges, Item matrix[])
     }
 }
 
-void MiniSpanTree_Prim(int matrix[][3]){
-
+int find_root(int x, int parent[]){
+    int x_root = x;
+    while(parent[x_root] != -1){
+        x_root = parent[x_root];
+    }
+    return x_root;
 }
 
-void MiniSpanTree_Kruskal(int matrix[][3])
-{
+int union_vertices(int x, int y, int parent[], int rank[]){
+    int x_root = find_root(x, parent);
+    int y_root = find_root(y, parent);
+    if (x_root == y_root)
+        return 0;
+    else{
+        if (rank[x_root]>rank[y_root])
+            parent[y_root] = x_root;
+        else if (rank[y_root]> rank[x_root])
+            parent[x_root] = y_root;
+        else{
+            parent[x_root] = y_root;
+            rank[y_root]++;
+        }
+        return 1;
+    }
+}
 
+void MiniSpanTree_Prim(Item matrix[], int vertexes, int edges)
+{
+    cout<<"...."<<endl;
+    return;
+}
+
+void MiniSpanTree_Kruskal(Item matrix[], int vertexes, int edges, int parent[], int rank[])
+{
+    int k=0;
+    int result=0;
+    for (int i = 0; i < edges; ++i) {
+        if (k == vertexes-1)
+            break;
+        if (union_vertices(matrix[i].from-1, matrix[i].to-1, parent, rank)){
+            result+=matrix[i].dis;
+            k++;
+        }
+    }
+    cout<<result<<endl;
+    return;
 }
 
 int main()
@@ -43,9 +88,13 @@ int main()
     int edges, vertexes;
     cin>>vertexes>>edges;
     Item matrix[edges];
+    Input(edges, matrix);
+    MiniSpanTree_Prim(matrix, vertexes, edges);
     int parent[vertexes];
-    Input(vertexes, edges, matrix, parent);
+    int rank[vertexes];
     sort(edges, matrix);
+    Init_tree(vertexes, parent, rank);
+    MiniSpanTree_Kruskal(matrix, vertexes, edges, parent, rank);
 
     return 0;
 }
