c48457eabbef5a3939be071e395d566c2646416a
LTstrange <835422774@qq.com>
2019-05-29 22:15:48 +0800

    finsh 14_2.cpp 's insert function.(unfinished)

--- datastruct_14/datastruct_14_2.cpp
+++ datastruct_14/datastruct_14_2.cpp
@@ -2,17 +2,50 @@
 
 using namespace std;
 
-typedef struct Node{
+typedef struct Node {
     int data;
     Node *Lchild;
     Node *Rchild;
-}BinTree, *PNode;
+} *BinTree;
+
+void Insert(BinTree root, int data) {
+    if (!root->data) {
+        root->data = data;
+    } else {
+        if (data > root->data) {
+            if (root->Rchild == NULL) {
+                root->Rchild = (BinTree) malloc(sizeof(Node));
+            }
+            Insert(root->Rchild, data);
+        } else {
+            if (root->Lchild == NULL) {
+                root->Lchild = (BinTree) malloc(sizeof(Node));
+            }
+            Insert(root->Lchild, data);
+        }
+    }
+}
+
+void Delete(BinTree root, int target) {
+    if (root->data == target)
+    {
+
+    }
+}
 
 int main() {
     int x;
-    while (cin>>x){
-        if (!x) break;
+    BinTree root;
+    root = (BinTree)malloc(sizeof(Node));
 
+    while (cin >> x) {
+        if (!x) break;
+        Insert(root, x);
     }
+
+    int target, finddata;
+
+    cin>>target>>finddata;
+
     return 0;
 }
