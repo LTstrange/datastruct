f4007eec30d2e38394dc99d3106cc625c85ef17d
LTstrange <835422774@qq.com>
2019-05-30 19:23:39 +0800

    finsh 14_2.cpp!

--- datastruct_14/datastruct_14_2.cpp
+++ datastruct_14/datastruct_14_2.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <malloc.h>
 
 using namespace std;
 
@@ -13,12 +14,12 @@ void Insert(BinTree root, int data) {
         root->data = data;
     } else {
         if (data > root->data) {
-            if (root->Rchild == NULL) {
+            if (!root->Rchild) {
                 root->Rchild = (BinTree) malloc(sizeof(Node));
             }
             Insert(root->Rchild, data);
         } else {
-            if (root->Lchild == NULL) {
+            if (!root->Lchild) {
                 root->Lchild = (BinTree) malloc(sizeof(Node));
             }
             Insert(root->Lchild, data);
@@ -26,17 +27,57 @@ void Insert(BinTree root, int data) {
     }
 }
 
-void Delete(BinTree root, int target) {
-    if (root->data == target)
-    {
-
+void Delete(BinTree &root, int target) {
+    BinTree p, f, s, q;
+    p = root;
+    f = NULL;
+    while (p) {
+        if (p->data == target) break;
+        f = p;
+        if (p->data > target) p = p->Lchild;
+        else p = p->Rchild;
     }
+    if (!p) return;
+    if (!p->Lchild) {
+        if (!f) root = p->Rchild;
+        else if (f->Lchild == p) f->Lchild = p->Rchild;
+        else f->Rchild = p->Rchild;
+        free(p);
+    } else {
+        q = p;
+        s = p->Lchild;
+        while (s->Rchild) {
+            q = s;
+            s = s->Rchild;
+        }
+        if (q == p) q->Lchild = s->Lchild;
+        else q->Rchild = s->Lchild;
+        p->data = s->data;
+        free(s);
+    }
+}
+
+void LDR(BinTree root) {
+    if (root) {
+        LDR(root->Lchild);
+        cout << root->data << ' ';
+        LDR(root->Rchild);
+    }
+}
+
+void find(BinTree root, int finddata, int depth) {
+    if (root->data == finddata){
+        cout<<depth+1<<endl;
+        return;
+    }
+    if (root->Rchild) find(root->Rchild, finddata, depth+1);
+    if (root->Lchild) find(root->Lchild, finddata, depth+1);
 }
 
 int main() {
     int x;
     BinTree root;
-    root = (BinTree)malloc(sizeof(Node));
+    root = (BinTree) malloc(sizeof(Node));
 
     while (cin >> x) {
         if (!x) break;
@@ -45,7 +86,16 @@ int main() {
 
     int target, finddata;
 
-    cin>>target>>finddata;
+    cin >> target >> finddata;
+
+//    LDR(root);
+//    cout<<endl;
+    Delete(root, target);
+
+    LDR(root);
+    cout<<endl;
+
+    find(root, finddata, 0);
 
     return 0;
 }
