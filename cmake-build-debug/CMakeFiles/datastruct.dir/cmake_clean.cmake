1f958ecca59182cc6ced4d278d0d9576058bb8ac
LTstrange <835422774@qq.com>
2019-06-06 12:45:09 +0800

    添加了编程环境的文件，以便在其他机器上也可以使用

---
+++ cmake-build-debug/CMakeFiles/datastruct.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/datastruct.dir/datastruct_15/datastruct_15_1.cpp.o"
+  "datastruct.pdb"
+  "datastruct"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/datastruct.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
